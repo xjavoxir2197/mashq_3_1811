24c3aa9f9f032a48ffe7016ec4f436aa6bd16f5b
xjavoxir2197 <xjavoxir2197@gmail.com>
2025-11-18 15:19:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -17,13 +17,4 @@ class Cow(Animal):
     def speak(self):
         return "Moo!"
 
-# Foydalanish
-print("\n--- Hayvonlar ovozi (Polimorfizm) ---")
-animals = [
-    Dog("Katta it"),
-    Cat("Kichik mushuk"),
-    Cow("Katta sigir"),
-    Dog("Kichik it")
-]
-for animal in animals:
-    print(f"🔊 {animal.name} deydi: {animal.speak()}")
+
